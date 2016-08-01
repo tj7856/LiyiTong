4e16c9161dd0902ef311520346357dc1e303d388
geng <devgeng@sina.com>
2016-08-01 16:22:09 +0800

    更新8.1

---
+++ LiyiTongUITests/adButton.h
@@ -0,0 +1,13 @@
+//
+//  adButton.h
+//  LiyiTong
+//
+//  Created by 耿远风 on 16/8/1.
+//  Copyright © 2016年 WanYu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface adButton : UIView
+
+@end
